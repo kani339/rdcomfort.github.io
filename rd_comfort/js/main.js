3740620ff6ea3cfe824c28e1d8d8019222daa689
V_Kani <kanibeats@gmail.com>
2017-06-24 18:43:35 +0300

    move from main page to portfolio with active filter state

--- rd_comfort/js/main.js
+++ rd_comfort/js/main.js
@@ -1,13 +1,42 @@
 $(function(){
 	
-	$("#portfolio_grid").mixItUp();
 	
+	function activeItem() {
+		var sect = localStorage.getItem('section');
+		$('#portfolio_grid').mixItUp({
+			load: {
+				filter: '.'+sect
+			}
+		});	
+	}
+
+
+	$(".image-portfolio").on('click', function(){
+	 	var sect = $(this).attr("data-section");
+	 	localStorage.setItem("section", sect);
+	 	localStorage.setItem("state", "true");
+	  window.location = "portfolio.html";	 
+	});
+
+
+	if(localStorage.getItem("state")=="true") {
+       activeItem();
+   } else {
+    	$('#portfolio_grid').mixItUp({
+				load: {
+			      filter: '.office'
+				}
+			});
+    }
+    //default state
+    localStorage.setItem("state", "false");
+		
+
 	//Add uniq id for modal-window
 	$(".mix").each(function(i){
 		$(this).find("a").attr("data-target", "#portf_"+ i);
 		$(this).find(".fade").attr("id", "portf_" + i);
 	});
-
 
 	//take same image url
 	$(".mix").each(function(y){
@@ -16,9 +45,6 @@ $(function(){
 		$(".modal-body").find(l).attr("src", src);
 	});
 
-	
-	//Pop-up
-	$(".popup_content").magnificPopup({type : "inline", midClick: true});
 
 	var navOffset = $('.navbar').offset().top;
 
@@ -34,11 +60,6 @@ $(function(){
 			$(".navbar").removeClass("fixed");
 		}
 	});
-
-	//Remove lightbox after refreshing page
-	//history.pushState("", document.title, window.location.pathname);
 	
-
-
 	
 });
